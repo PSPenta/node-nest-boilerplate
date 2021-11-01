76b395df4dbaccc1121d2b8ede0df68506eeccbe
Pritesh Shinde <pritesh.s@purplle.com>
2021-11-01 23:04:50 +0530

    Best practice modules and security added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { LoggerModule } from 'nestjs-pino';
+import { StatusMonitorModule } from 'nestjs-status-monitor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    LoggerModule.forRoot(),
+    ConfigModule.forRoot(),
+    StatusMonitorModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bufferLogs: true,
+  });
+
+  await app.useLogger(app.get(Logger));
+  await app.use(helmet());
+  await app.use(compression());
+
+  const config = new DocumentBuilder()
+    .setTitle('Node Nest Boilerplate')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/api-docs', app, document);
+
   await app.listen(process.env.HTTP_PORT);
 }
 bootstrap();
