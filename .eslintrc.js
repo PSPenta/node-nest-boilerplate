06df543a2773f2e4cefed375f252f542fcda1a88
Pritesh Shinde <pritesh.shinde@rupyy.com>
2023-09-13 00:11:04 +0530

    Code optimised

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,42 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module'
+    project: "tsconfig.json",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'airbnb-base',
-    'airbnb-typescript/base'
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
   ],
   root: true,
   env: {
     node: true,
-    jest: true
+    jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    "@typescript-eslint/comma-dangle": 'off',
-    "@typescript-eslint/indent": 'off',
-    "import/prefer-default-export": 'off',
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/comma-dangle": "off",
+    "@typescript-eslint/indent": "off",
+    "import/prefer-default-export": "off",
     "prettier/prettier": 'off',
-    "comma-dangle": [
-      "error",
-      "never"
-    ],
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
+    "comma-dangle": ["error", "never"],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
     "no-console": [
       "error",
       {
-        "allow": [
-          "error",
-          "info",
-          "warn"
-        ]
-      }
+        allow: ["error", "info", "warn"],
+      },
     ],
-    "no-multi-spaces": [
-      "error"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
-  }
+    "no-multi-spaces": ["error"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+  },
 };
